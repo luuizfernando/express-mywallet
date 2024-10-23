ce4d53e54ab1a1256f5bc6a2384cb1d3a3aff09e
Luiz Fernando <113379005+luuizfernando@users.noreply.github.com>
2024-10-23 15:32:15 -0300

    POST /sign-in feito

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import express from 'express';
 import joi from 'joi';
 import { MongoClient, ObjectId } from 'mongodb';
 import { v4 as uuid } from 'uuid';
+import { signUp } from './controllers/usersController.js';
 
 const app = express();
 
@@ -20,18 +21,18 @@ try {
 } catch (err) {
     console.log(err.message);
 }
-const db = mongoClient.db();
+export const db = mongoClient.db();
 
 // Schemas
 const userSchema = joi.object({
-    name: joi.string().required(),
+    userName: joi.string().required(),
     email: joi.string().email().required(),
     password: joi.string().required().min(3)
 });
 
 // EndPoints
 app.post("/sign-up", async (req, res) => {
-    const { name, email, password } = req.body;
+    const { userName, email, password } = req.body;
 
     const validation = userSchema.validate(req.body, { abortEarly: false });
     if (validation.error) {
@@ -45,15 +46,31 @@ app.post("/sign-up", async (req, res) => {
 
         const hash = bcrypt.hashSync(password, 10);
 
-        await db.collection("usuarios").insertOne({ name, email, password: hash });
+        await db.collection("usuarios").insertOne({ userName, email, password: hash });
         res.sendStatus(201);
     } catch (err) {
         res.status(500).send(err.message);
     }
 });
 
-app.post("/", async (req, res) => {
+app.post("/sign-in", async (req, res) => {
+    const { email, password } = req.body;
 
+    try {
+        const user = await db.collection("usuarios").findOne({ email });
+        if (!user) return res.status(404).send("Usuário não encontrado");
+
+        const correctPassword = bcrypt.compareSync(password, user.password);
+        if (!correctPassword) return res.status(401).send("Senha incorreta");
+
+        const token = uuid();
+        await db.collection("sections").insertOne({ token, userId: user._id });
+        
+        const { userName } = user;
+        res.send({ token, userName });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 });
 
 app.post("/nova-transacao/:tipo", (req, res) => {
---
+++ src/controllers/usersController.js
@@ -0,0 +1,29 @@
+import bcrypt from 'bcrypt';
+import { db } from '../app.js';
+import { v4 as uuid } from 'uuid';
+
+export async function signUp(req, res) {
+    const { name, email, password } = req.body;
+
+    const validation = userSchema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try {
+        const user = await db.collection("usuarios").findOne({ email });
+        if (user) return res.status(409).send("E-mail já cadastrado");
+
+        const hash = bcrypt.hashSync(password, 10);
+
+        await db.collection("usuarios").insertOne({ name, email, password: hash });
+        res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+};
+
+export async function signIn(req, res) {
+    
+};
