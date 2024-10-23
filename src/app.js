a5d153f7946ae97bd980c87046b78f09693a15a5
Luiz Fernando <113379005+luuizfernando@users.noreply.github.com>
2024-10-23 12:10:40 -0300

    POST /sign-up feito

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
-import express from 'express';
+import bcrypt from 'bcrypt';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import express from 'express';
 import joi from 'joi';
+import { MongoClient, ObjectId } from 'mongodb';
+import { v4 as uuid } from 'uuid';
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 dotenv.config();
 
+// DB Config
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 try {
     await mongoClient.connect();
@@ -16,8 +22,34 @@ try {
 }
 const db = mongoClient.db();
 
-app.post("/cadastro", async (req, res) => {
+// Schemas
+const userSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().required().min(3)
+});
 
+// EndPoints
+app.post("/sign-up", async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const validation = userSchema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try {
+        const user = await db.collection("usuarios").findOne({ email });
+        if (user) return res.status(409).send("E-mail já cadastrado");
+
+        const hash = bcrypt.hashSync(password, 10);
+
+        await db.collection("usuarios").insertOne({ name, email, password: hash });
+        res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 });
 
 app.post("/", async (req, res) => {
@@ -31,3 +63,5 @@ app.post("/nova-transacao/:tipo", (req, res) => {
 app.get("/home", async (req, res) => {
 
 });
+
+app.listen(process.env.PORT, () => console.log(`Servidor rodando na porta ${process.env.PORT}.`));
