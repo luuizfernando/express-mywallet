efb47a01cbd135c0f7e51f1638d1a96764322ea0
Luiz Fernando <113379005+luuizfernando@users.noreply.github.com>
2024-10-26 15:04:25 -0300

    transactions controller

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
-import bcrypt from 'bcrypt';
+
 import cors from 'cors';
-import dayjs from 'dayjs';
 import dotenv from 'dotenv';
 import express from 'express';
-import joi from 'joi';
 import { signIn, signUp } from './controllers/users.controller.js';
+import { makeTransaction, returnTransactions } from './controllers/transactions.controller.js';
 
 const app = express();
 
@@ -12,60 +11,13 @@ app.use(cors());
 app.use(express.json());
 dotenv.config();
 
-// Schemas
-
-const transactionSchema = joi.object({
-    value: joi.number().precision(2).positive().required(),
-    description: joi.string()
-});
-
 // EndPoints
 app.post("/sign-up", signUp);
 
 app.post("/sign-in", signIn);
 
-app.post("/nova-transacao/:tipo", async (req, res) => {
-    const { tipo } = req.params;
-    const { value, description } = req.body;
-    const { authorization } = req.headers;
+app.post("/nova-transacao/:tipo", makeTransaction);
 
-    const today = dayjs().format('DD-MM-YYYY');
-
-    const token = authorization?.replace("Bearer ", "");
-    if (!token) return res.sendStatus(401);
-
-    try {
-        const session = await db.collection("sections").findOne({ token });
-        if (!session) return res.sendStatus(401);
-
-        const validation = transactionSchema.validate(req.body, { abortEarly: false });
-        if (validation.error) {
-            const errors = validation.error.details.map((detail) => detail.message);
-            return res.status(422).send(errors);
-        }
-
-        const transaction = await db.collection("transactions").insertOne({ today, value, description, tipo });
-        if (transaction) return res.sendStatus(201);
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-});
-
-app.get("/home", async (req, res) => {
-    const { authorization } = req.headers;
-
-    const token = authorization?.replace("Bearer ", "");
-    if (!token) return res.sendStatus(401);
-
-    try {
-        const sessao = await db.collection("sections").findOne({ token });
-        if (!sessao) return res.sendStatus(401);
-
-        const transactions = await db.collection("transactions").find().toArray();
-        res.send(transactions);
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-});
+app.get("/home", returnTransactions);
 
 app.listen(process.env.PORT, () => console.log(`Servidor rodando na porta ${process.env.PORT}.`));
---
+++ src/controllers/transactions.controller.js
@@ -0,0 +1,52 @@
+import joi from 'joi';
+import dayjs from 'dayjs';
+import { db } from '../database/database.connection.js';
+
+const transactionSchema = joi.object({
+    value: joi.number().precision(2).positive().required(),
+    description: joi.string()
+});
+
+export async function makeTransaction(req, res) {
+    const { tipo } = req.params;
+    const { value, description } = req.body;
+    const { authorization } = req.headers;
+
+    const today = dayjs().format('DD-MM-YYYY');
+
+    const token = authorization?.replace("Bearer ", "");
+    if (!token) return res.sendStatus(401);
+
+    try {
+        const session = await db.collection("sections").findOne({ token });
+        if (!session) return res.sendStatus(401);
+
+        const validation = transactionSchema.validate(req.body, { abortEarly: false });
+        if (validation.error) {
+            const errors = validation.error.details.map((detail) => detail.message);
+            return res.status(422).send(errors);
+        }
+
+        const transaction = await db.collection("transactions").insertOne({ today, value, description, tipo });
+        if (transaction) return res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+};
+
+export async function returnTransactions(req, res) {
+    const { authorization } = req.headers;
+
+    const token = authorization?.replace("Bearer ", "");
+    if (!token) return res.sendStatus(401);
+
+    try {
+        const sessao = await db.collection("sections").findOne({ token });
+        if (!sessao) return res.sendStatus(401);
+
+        const transactions = await db.collection("transactions").find().toArray();
+        res.send(transactions);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+};
