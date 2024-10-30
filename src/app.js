7cc35cadd5251989bd494d069d33338b53bcbd33
Luiz Fernando <113379005+luuizfernando@users.noreply.github.com>
2024-10-29 21:36:58 -0300

    alter port

--- src/app.js
+++ src/app.js
@@ -8,4 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.PORT, () => console.log(`Servidor rodando na porta ${process.env.PORT}.`));
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}.`));
