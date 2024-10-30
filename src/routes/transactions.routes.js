019dd4ee435dcc0b7869af08207754b6179ac1fc
Luiz Fernando <113379005+luuizfernando@users.noreply.github.com>
2024-10-29 21:22:17 -0300

    Correcao schemas

--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { createTransaction, getTransactions } from "../controllers/transaction.controllers.js";
 import { authValidation } from "../middlewares/authValidation.middleware.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
-import { transactionSchema } from "../schemas/transaction.schemas.js";
+import { transactionSchema } from "../schemas/transactions.schemas.js";
 
 const transactionRouter = Router();
 
