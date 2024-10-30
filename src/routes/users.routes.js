57c010fc210e04843786082ee28b0c7c01e2d4f8
Luiz Fernando <113379005+luuizfernando@users.noreply.github.com>
2024-10-29 21:12:14 -0300

    Alteracao authValidation

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { logout, signIn, signUp } from '../controllers/auth.controllers.js';
 import { validateSchema } from '../middlewares/validateSchema.middleware.js';
 import { loginSchema, userSchema } from '../schemas/users.schema.js';
-import { authValidation } from '../middlewares/auth.middleware.js';
+import { authValidation } from '../middlewares/authValidation.middleware.js';
 
 const usersRouter = Router();
 
